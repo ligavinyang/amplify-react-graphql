74e17ea004f6593a63dfdd6af41df3ae00ac3f07
Gavin <ligavinyang@gmail.com>
2024-03-07 00:15:35 +0900

    module5

--- src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,5 +4,23 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string"
     }
+  },
+  "auth": {
+    "amplifyreactgraphqlf1eab480": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "learningStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
